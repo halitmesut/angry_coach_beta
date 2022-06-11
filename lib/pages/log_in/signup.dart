9ffa33f072d6267ea1d5db994d16a8acd49b4d56
halitmesut <halitmesut@gmail.com>
2022-06-11 03:55:52 +0300

    validation optimized

--- lib/pages/log_in/signup.dart
+++ lib/pages/log_in/signup.dart
@@ -45,151 +45,139 @@ class _SignUpPageState extends State<SignUpPage> {
         padding: EdgeInsets.symmetric(horizontal: 40),
         height: MediaQuery.of(context).size.height,
         width: double.infinity,
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: [
-            Text(
-              "Create And Accound. It's Free.",
-              style: TextStyle(
-                fontSize: 15,
-                color: Colors.grey[700],
-              ),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Text(
-              "Email",
-              style: TextStyle(
-                fontSize: 15,
-                fontWeight: FontWeight.w400,
-                color: Colors.black87,
-              ),
-            ),
-            SizedBox(
-              height: 5,
-            ),
-            TextFormField(
-              controller: emailController,
-              obscureText: false,
-              validator: (email) {
-                email != null && !EmailValidator.validate(email)
-                    ? "Enter a valid Email"
-                    : null;
-              },
-              decoration: InputDecoration(
-                contentPadding:
-                    EdgeInsets.symmetric(vertical: 0, horizontal: 10),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.grey[400]!,
-                  ),
-                ),
-                border: OutlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.grey[400]!,
-                  ),
+        child: Form(
+          key: formKey,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Text(
+                "Create And Accound. It's Free.",
+                style: TextStyle(
+                  fontSize: 15,
+                  color: Colors.grey[700],
                 ),
               ),
-            ),
-            SizedBox(
-              height: 25,
-            ),
-            Text(
-              "Password",
-              style: TextStyle(
-                fontSize: 15,
-                fontWeight: FontWeight.w400,
-                color: Colors.black87,
+              SizedBox(
+                height: 20,
               ),
-            ),
-            SizedBox(
-              height: 5,
-            ),
-            TextFormField(
-              controller: passwordController,
-              obscureText: true,
-              autovalidateMode: AutovalidateMode.onUserInteraction,
-              validator: (value) {
-                value != null && value.length < 6
+              SizedBox(
+                height: 5,
+              ),
+              TextFormField(
+                controller: emailController,
+                decoration: InputDecoration(
+                  labelText: "Email",
+                  contentPadding:
+                      EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.grey[400]!,
+                    ),
+                  ),
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.grey[400]!,
+                    ),
+                  ),
+                ),
+                textInputAction: TextInputAction.next,
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                validator: (email) =>
+                    email != null && !EmailValidator.validate(email)
+                        ? "Enter a valid Email"
+                        : null,
+              ),
+              SizedBox(
+                height: 25,
+              ),
+              TextFormField(
+                controller: passwordController,
+                obscureText: true,
+                textInputAction: TextInputAction.next,
+                autovalidateMode: AutovalidateMode.onUserInteraction,
+                validator: (value) => value != null && value.length < 6
                     ? "Enter min 6 characters"
-                    : null;
-              },
-              decoration: InputDecoration(
-                contentPadding:
-                    EdgeInsets.symmetric(vertical: 0, horizontal: 10),
-                enabledBorder: OutlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.grey[400]!,
+                    : null,
+                decoration: InputDecoration(
+                  labelText: "Password",
+                  contentPadding:
+                      EdgeInsets.symmetric(vertical: 0, horizontal: 10),
+                  enabledBorder: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.grey[400]!,
+                    ),
                   ),
-                ),
-                border: OutlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Colors.grey[400]!,
+                  border: OutlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Colors.grey[400]!,
+                    ),
                   ),
                 ),
               ),
-            ),
-            SizedBox(
-              height: 25,
-            ),
-            Container(
-              padding: EdgeInsets.only(top: 2, left: 2),
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(50),
-                border: Border(
-                  bottom: BorderSide(color: Colors.black),
-                  top: BorderSide(color: Colors.black),
-                  left: BorderSide(color: Colors.black),
-                  right: BorderSide(color: Colors.black),
-                ),
+              SizedBox(
+                height: 25,
               ),
-              child: MaterialButton(
-                minWidth: double.infinity,
-                elevation: 0,
-                color: Colors.deepOrange,
-                onPressed: signUp,
-                height: 60,
-                child: Text(
-                  "Sign Up",
-                  style: TextStyle(fontWeight: FontWeight.w600, fontSize: 22),
-                ),
-                shape: RoundedRectangleBorder(
+              Container(
+                padding: EdgeInsets.only(top: 2, left: 2),
+                decoration: BoxDecoration(
                   borderRadius: BorderRadius.circular(50),
+                  border: Border(
+                    bottom: BorderSide(color: Colors.black),
+                    top: BorderSide(color: Colors.black),
+                    left: BorderSide(color: Colors.black),
+                    right: BorderSide(color: Colors.black),
+                  ),
+                ),
+                child: MaterialButton(
+                  minWidth: double.infinity,
+                  elevation: 0,
+                  color: Colors.deepOrange,
+                  onPressed: signUp,
+                  height: 60,
+                  child: Text(
+                    "Sign Up",
+                    style: TextStyle(fontWeight: FontWeight.w600, fontSize: 22),
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(50),
+                  ),
                 ),
               ),
-            ),
-            RichText(
-              text: TextSpan(
-                text: "Naccount",
-                style: TextStyle(color: Colors.black, fontSize: 15),
-                children: [
-                  TextSpan(
-                      recognizer: TapGestureRecognizer()
-                        ..onTap = widget.onClickedSignIn,
-                      text: "Sign In",
-                      style: TextStyle(
-                          decoration: TextDecoration.underline,
-                          color: Colors.orange))
-                ],
-              ),
-            ),
-            Container(
-              height: MediaQuery.of(context).size.height / 3,
-              decoration: BoxDecoration(
-                image: DecorationImage(
-                  image: AssetImage("assets/angrycoachh.jpg"),
-                  fit: BoxFit.fitHeight,
+              RichText(
+                text: TextSpan(
+                  text: "Naccount",
+                  style: TextStyle(color: Colors.black, fontSize: 15),
+                  children: [
+                    TextSpan(
+                        recognizer: TapGestureRecognizer()
+                          ..onTap = widget.onClickedSignIn,
+                        text: "Sign In",
+                        style: TextStyle(
+                            decoration: TextDecoration.underline,
+                            color: Colors.orange))
+                  ],
                 ),
               ),
-            )
-          ],
+              Container(
+                height: MediaQuery.of(context).size.height / 3,
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage("assets/angrycoachh.jpg"),
+                    fit: BoxFit.fitHeight,
+                  ),
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
   }
 
   Future signUp() async {
+    final isValid = formKey.currentState!.validate();
+    if (!isValid) return;
+
     showDialog(
         context: context,
         barrierDismissible: false,
@@ -205,8 +193,6 @@ class _SignUpPageState extends State<SignUpPage> {
     navigatorKey.currentState!.popUntil((route) => route.isFirst);
   }
 }
-// final isValid = formKey.currentState!.validate();
-//  if (!isValid) return;
 
 
 
