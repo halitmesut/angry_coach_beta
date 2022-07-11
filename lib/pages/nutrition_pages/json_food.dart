d85bcd638aaab4fe2831e5db2c748684cc24ddcf
halitmesut <halitmesut@gmail.com>
2022-07-12 02:42:32 +0300

    food class created

--- lib/pages/nutrition_pages/json_food.dart
+++ lib/pages/nutrition_pages/json_food.dart
@@ -27,47 +27,55 @@ class _JsonFoodState extends State<JsonFood> {
               return ListView.builder(
                   itemCount: food == null ? 0 : food.length,
                   itemBuilder: (BuildContext context, int index) {
-                    return Card(
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.stretch,
-                        children: [
-                          Text('name = ' + food[index]["description"]),
-                          Text('portion Description = ' +
-                              food[index]["foodPortions"][0]
-                                  ["portionDescription"]),
-                          Text('gram Weight = ' +
-                              food[index]["foodPortions"][0]["gramWeight"]
-                                  .toString()),
-                          Text(food[index]["foodNutrients"][0]["nutrient"]
-                                  ["name"] +
-                              ' = ' +
-                              food[index]["foodNutrients"][0]["amount"]
-                                  .toString() +
-                              food[index]["foodNutrients"][0]["nutrient"]
-                                  ["unitName"]),
-                          Text(food[index]["foodNutrients"][1]["nutrient"]
-                                  ["name"] +
-                              ' = ' +
-                              food[index]["foodNutrients"][1]["amount"]
-                                  .toString() +
-                              food[index]["foodNutrients"][1]["nutrient"]
-                                  ["unitName"]),
-                          Text(food[index]["foodNutrients"][2]["nutrient"]
-                                  ["name"] +
-                              ' = ' +
-                              food[index]["foodNutrients"][2]["amount"]
-                                  .toString() +
-                              food[index]["foodNutrients"][2]["nutrient"]
-                                  ["unitName"]),
-                          Text(food[index]["foodNutrients"][3]["nutrient"]
-                                  ["name"] +
-                              ' = ' +
-                              food[index]["foodNutrients"][3]["amount"]
-                                  .toString() +
-                              food[index]["foodNutrients"][3]["nutrient"]
-                                  ["unitName"])
-                        ],
+                    return GestureDetector(
+                      child: Card(
+                        child: Column(
+                          crossAxisAlignment: CrossAxisAlignment.stretch,
+                          children: [
+                            Text('name = ${food[index]["description"]}'),
+                            Text(
+                                'portion Description = ${food[index]["foodPortions"][0]["portionDescription"]}'),
+                            Text(
+                                'gram Weight = ${food[index]["foodPortions"][0]["gramWeight"]}'),
+                            Text(food[index]["foodNutrients"][0]["nutrient"]
+                                    ["name"] +
+                                ' = ' +
+                                food[index]["foodNutrients"][0]["amount"]
+                                    .toString() +
+                                food[index]["foodNutrients"][0]["nutrient"]
+                                    ["unitName"]),
+                            Text(food[index]["foodNutrients"][1]["nutrient"]
+                                    ["name"] +
+                                ' = ' +
+                                food[index]["foodNutrients"][1]["amount"]
+                                    .toString() +
+                                food[index]["foodNutrients"][1]["nutrient"]
+                                    ["unitName"]),
+                            Text(food[index]["foodNutrients"][2]["nutrient"]
+                                    ["name"] +
+                                ' = ' +
+                                food[index]["foodNutrients"][2]["amount"]
+                                    .toString() +
+                                food[index]["foodNutrients"][2]["nutrient"]
+                                    ["unitName"]),
+                            Text(food[index]["foodNutrients"][3]["nutrient"]
+                                    ["name"] +
+                                ' = ' +
+                                food[index]["foodNutrients"][3]["amount"]
+                                    .toString() +
+                                food[index]["foodNutrients"][3]["nutrient"]
+                                    ["unitName"]),
+                            Text('fdcId = ${food[index]["fdcId"]}'),
+                          ],
+                        ),
                       ),
+                      onTap: () {
+                        for (var index = 0; index < food.length; index++) {
+                          debugPrint(
+                              '(description: "${food[index]["description"]}", portionDescription: "${food[index]["foodPortions"][0]["portionDescription"]}", gramWeight: ${food[index]["foodPortions"][0]["gramWeight"]}, gramProtein: ${food[index]["foodNutrients"][0]["amount"]}, gramFat: ${food[index]["foodNutrients"][1]["amount"]}, gramCarbohydrate: ${food[index]["foodNutrients"][2]["amount"]}, kcalEnergy: ${food[index]["foodNutrients"][3]["amount"]}, fdcId: ${food[index]["fdcId"]}),');
+                        }
+                        ;
+                      },
                     );
                   });
             },
