57b43a44807e6c5b7eb53f293ebec1e38031178b
halitmesut <halitmesut@gmail.com>
2022-07-30 04:22:38 +0300

    me page changed

--- lib/pages/me_page.dart
+++ lib/pages/me_page.dart
@@ -1,11 +1,25 @@
+import 'package:angry_coach_beta/extract/my_button.dart';
 import 'package:angry_coach_beta/extract/widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:intl/intl.dart';
 
-class MePage extends StatelessWidget {
+class MePage extends StatefulWidget {
   const MePage({Key? key}) : super(key: key);
 
   @override
+  State<MePage> createState() => _MePageState();
+}
+
+class _MePageState extends State<MePage> {
+  var userPropertiesBox = Hive.box("userProperties");
+  var userDailyValuesBox = Hive.box("userDailyValues");
+
+  @override
   Widget build(BuildContext context) {
+    var dayTime = userDailyValuesBox.get('dayTime');
+    var allWeights = userDailyValuesBox.get("weight");
+
     return Scaffold(
       body: Padding(
         padding: const EdgeInsets.fromLTRB(10, 10, 10, 0),
@@ -67,7 +81,132 @@ class MePage extends StatelessWidget {
               flex: 20,
               child: GestureDetector(
                 onTap: () {
-                  bottomSheetContainer(context, const Text("holaaaaaa"));
+                  double sliderAmount =
+                      userDailyValuesBox.get('weight').last['weight'];
+                  // double sliderAmount = userPropertiesBox.get('weight');
+                  showModalBottomSheet(
+                      context: context,
+                      builder: (context) => Center(
+                            child: Container(
+                              padding: const EdgeInsets.all(40),
+                              child: Column(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceEvenly,
+                                children: [
+                                  StatefulBuilder(
+                                    builder: (context, state) {
+                                      return Column(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceBetween,
+                                        children: [
+                                          Row(
+                                            mainAxisAlignment:
+                                                MainAxisAlignment.center,
+                                            crossAxisAlignment:
+                                                CrossAxisAlignment.baseline,
+                                            textBaseline:
+                                                TextBaseline.alphabetic,
+                                            children: [
+                                              const SizedBox(
+                                                width: 30.0,
+                                              ),
+                                              Text(
+                                                sliderAmount.toStringAsFixed(1),
+                                                style: const TextStyle(
+                                                  fontSize: 30.0,
+                                                  fontWeight: FontWeight.bold,
+                                                  color: Colors.black,
+                                                ),
+                                              ),
+                                              const Text(
+                                                "kg",
+                                                style: TextStyle(
+                                                  fontSize: 20.0,
+                                                  fontWeight: FontWeight.bold,
+                                                  color: Colors.black,
+                                                ),
+                                              )
+                                            ],
+                                          ),
+                                          const SizedBox(
+                                            height: 20,
+                                          ),
+                                          Slider(
+                                            value: sliderAmount,
+                                            min: userDailyValuesBox
+                                                    .get('weight')
+                                                    .last['weight'] -
+                                                2.5,
+                                            max: userDailyValuesBox
+                                                    .get('weight')
+                                                    .last['weight'] +
+                                                2.5,
+                                            activeColor: Colors.deepOrange,
+                                            inactiveColor: const Color.fromARGB(
+                                                255, 255, 193, 174),
+                                            onChanged: (double newValue) {
+                                              state(() {
+                                                sliderAmount = newValue;
+                                              });
+                                              setState(() {
+                                                sliderAmount = newValue;
+                                              });
+                                            },
+                                          ),
+                                          const SizedBox(
+                                            height: 40,
+                                          ),
+                                          MyButton(
+                                            onPressedFunction: () async {
+                                              debugPrint(dayTime);
+
+                                              if (allWeights[allWeights.length -
+                                                      1]['date'] ==
+                                                  userDailyValuesBox
+                                                      .get('dayTime')) {
+                                                allWeights[
+                                                    allWeights.length - 1] = {
+                                                  'date': Hive.box(
+                                                          "userDailyValues")
+                                                      .get('dayTime'),
+                                                  'weight': sliderAmount
+                                                };
+
+                                                await userDailyValuesBox.put(
+                                                    "weight", allWeights);
+                                              } else {
+                                                allWeights.add(
+                                                  {
+                                                    'date': Hive.box(
+                                                            "userDailyValues")
+                                                        .get('dayTime'),
+                                                    'weight': sliderAmount
+                                                  },
+                                                );
+                                                await userDailyValuesBox.put(
+                                                    "weight", allWeights);
+                                              }
+
+                                              Navigator.of(context).pop();
+                                              debugPrint(allWeights.toString());
+                                            },
+                                            textTop: '',
+                                            textBottom: '',
+                                            text: "Save my new weight",
+                                            buttonColor: Colors.deepOrange,
+                                          ),
+                                        ],
+                                      );
+                                    },
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                      shape: const RoundedRectangleBorder(
+                        borderRadius:
+                            BorderRadius.vertical(top: Radius.circular(30)),
+                      ));
                 },
                 child: Container(
                   padding:
@@ -80,12 +219,17 @@ class MePage extends StatelessWidget {
                         topLeft: Radius.circular(30),
                         topRight: Radius.circular(30)),
                   ),
-                  child: const Center(
-                    child: Text(
-                      "84.4 kg",
-                      style: TextStyle(fontSize: 40),
-                      textAlign: TextAlign.center,
-                    ),
+                  child: Center(
+                    child: ValueListenableBuilder(
+                        valueListenable:
+                            Hive.box("userDailyValues").listenable(),
+                        builder: (context, Box box, _) {
+                          return Text(
+                            box.get('weight').last['weight'].toStringAsFixed(1),
+                            style: const TextStyle(fontSize: 40),
+                            textAlign: TextAlign.center,
+                          );
+                        }),
                   ),
                 ),
               ),
@@ -110,7 +254,8 @@ class MePage extends StatelessWidget {
                       leftTitle: "Başlangıç",
                       middleTitle: "Haftalık Hedef",
                       rightTitle: "Aylık Hedef",
-                      leftInput: 89.5,
+                      leftInput: Hive.box("userDailyValues").get('weight')[0]
+                          ['weight'],
                       middleInput: 83.5,
                       rightInput: 80.5),
                 ),
